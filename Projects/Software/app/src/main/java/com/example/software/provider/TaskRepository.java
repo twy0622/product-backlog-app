2c6a2e140c7745e93d3bbb004464569184ce561e
lzhaur1513 <lzhaur1513@gmail.com>
2022-09-01 16:00:50 +0800

    task database

---
+++ Projects/Software/app/src/main/java/com/example/software/provider/TaskRepository.java
@@ -0,0 +1,35 @@
+package com.example.software.provider;
+
+import android.app.Application;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class TaskRepository {
+    private TaskDao mTaskDao;
+    private LiveData<List<Task>> mAllTasks;
+
+    TaskRepository(Application application) {
+        TaskDatabase db = TaskDatabase.getDatabase(application);
+        mTaskDao = db.taskDao();
+        mAllTasks = mTaskDao.getAllTask();
+    }
+
+    LiveData<List<Task>> getAllTasks() {
+        return mAllTasks;
+    }
+
+    void insert(Task task) {
+        TaskDatabase.databaseWriteExecutor.execute(() -> mTaskDao.addTask(task));
+    }
+
+    void deleteTaskName(String name){
+        TaskDatabase.databaseWriteExecutor.execute(() -> mTaskDao.deleteTaskName(name));
+    }
+
+    void deleteAll(){
+        TaskDatabase.databaseWriteExecutor.execute(()->{
+            mTaskDao.deleteAllMovies();
+        });
+    }
+}
