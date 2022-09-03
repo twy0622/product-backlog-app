7b74d0f34f15e9f593a6e189364a30d20a5589c0
yeevern <yeevernchang@gmail.com>
2022-09-03 18:23:31 +0800

    tried to do the edit and delete button function for product backlogs. Still not fully done.

--- Projects/Software/app/src/main/java/com/example/software/EditTask.java
+++ Projects/Software/app/src/main/java/com/example/software/EditTask.java
@@ -1,14 +1,38 @@
 package com.example.software;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.software.provider.DemoAdapter;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class EditTask extends AppCompatActivity {
+
+    String []data = {"Hello", "Hii", "welcome"};
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_backlog);
+
+        List<String> items = new LinkedList<>();
+        items.add("Code It");
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        DemoAdapter adapter = new DemoAdapter(items);
+        recyclerView.setAdapter(adapter);
+
+        findViewById(R.id.add).setOnClickListener(view -> {
+            items.add(data[counter%3]);
+            counter++;
+            adapter.notifyItemInserted(items.size()-1);
+        });
     }
 }
