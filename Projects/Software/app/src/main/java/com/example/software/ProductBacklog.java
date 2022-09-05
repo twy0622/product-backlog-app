80454cd247a98dcaeb82bcf0f82110f66dc9b238
Iliyana <isam0004@student.monash.edu>
2022-09-05 14:13:29 +0800

    added tag filtering dropdown list into product backlog

--- Projects/Software/app/src/main/java/com/example/software/ProductBacklog.java
+++ Projects/Software/app/src/main/java/com/example/software/ProductBacklog.java
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.software.provider.TaskViewModel;
 
@@ -20,6 +21,16 @@ public class ProductBacklog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_backlog);
+
+        //spinner for filtering tags
+        Spinner tagFilterSpinner = findViewById(R.id.filterTagBox);
+        ArrayAdapter<String> filterTagAdapter = new ArrayAdapter<String>(ProductBacklog.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.filterTags));
+        filterTagAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tagFilterSpinner.setAdapter(filterTagAdapter);
+
+
+//        Button applyFilter = findViewById(R.id.applyFilterButton);
 
         recyclerView = findViewById(R.id.task_recycler_view);
 
