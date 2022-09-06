db53b71214fb65a6ecbe06e332f119de675bfb84
Justin Lai <jlai0040@student.monash.edu>
2022-09-06 23:39:36 +0800

    5th Update to Navigation Drawer UI with testing involved with the emulator. The Navigation Drawer can now successfully move to another activities but we need to keep implementing the navigation drawer to all of the pages. Unless someone knows a better alternative method?

--- Projects/Software/app/src/main/java/com/example/software/AddTask.java
+++ Projects/Software/app/src/main/java/com/example/software/AddTask.java
@@ -6,12 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class AddTask extends AppCompatActivity {
     private DrawerLayout drawer;
@@ -19,17 +20,44 @@ public class AddTask extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_task);
+        setContentView(R.layout.drawer_layout);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        drawer = findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.goToAddTask) {
+                    Intent addTaskIntent = new Intent(getApplicationContext(), AddTask.class);
+                    startActivity(addTaskIntent);
+                } else if (id == R.id.goToProductBackLog) {
+                    Intent productBackLogIntent = new Intent(getApplicationContext(), ProductBacklog.class);
+                    startActivity(productBackLogIntent);
+                } //else if (id == R.id.goToSprintBoard) {
+//            Intent sprintBoardIntent = new Intent(getApplicationContext(), SprintBoard.class);
+//            startActivity(sprintBoardIntent);
+                //} else if (id == R.id.goToTeamMembers) {
+//            Intent teamMembersIntent = new Intent(getApplicationContext(), TeamMembers.class);
+//            startActivity(teamMembersIntent);
+                //}
+
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+
     }
 
     @Override
@@ -40,25 +68,5 @@ public class AddTask extends AppCompatActivity {
        super.onBackPressed();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
 
-        if (id == R.id.goToAddTask) {
-            Intent addTaskIntent = new Intent(this, AddTask.class);
-            startActivity(addTaskIntent);
-        } else if (id == R.id.goToProductBackLog) {
-            Intent productBackLogIntent = new Intent(this, ProductBacklog.class);
-            startActivity(productBackLogIntent);
-        } //else if (id == R.id.goToSprintBoard) {
-//            Intent sprintBoardIntent = new Intent(this, SprintBoard.class);
-//            startActivity(sprintBoardIntent);
-        //} else if (id == R.id.goToTeamMembers) {
-//            Intent teamMembersIntent = new Intent(this, TeamMembers.class);
-//            startActivity(teamMembersIntent);
-        //}
-
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
