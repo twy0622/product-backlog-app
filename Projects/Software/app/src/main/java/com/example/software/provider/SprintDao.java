f3ee7c49d57ed604a04aff9f8f31982da743b479
Justin Lai <jlai0040@student.monash.edu>
2022-09-30 23:09:21 +0800

    Progress on the Backend of Add Sprint.

---
+++ Projects/Software/app/src/main/java/com/example/software/provider/SprintDao.java
@@ -0,0 +1,33 @@
+package com.example.software.provider;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SprintDao {
+
+    @Query("select * from sprints")
+    LiveData<List<Sprint>> getAllSprints();
+
+    @Query("select * from sprints where sprintName =:sprintName")
+    LiveData<List<Sprint>> getSprintName(String sprintName);
+
+    @Query("select * from sprints where sprintDate =:sprintDate")
+    LiveData<List<Sprint>> getSprintDate(String sprintDate);
+
+    @Insert
+    void addSprint(Sprint sprint);
+
+    @Query("delete from sprints where sprintID=:id")
+    void deleteSprintByID(int id);
+
+    @Query("delete FROM sprints")
+    void deleteAllSprints();
+
+    @Query("UPDATE sprints SET sprintName =:sprintName, sprintDate =:sprintDate WHERE sprintID =:id")
+    void updateSprintDetails(int id, String sprintName, String sprintDate);
+}
