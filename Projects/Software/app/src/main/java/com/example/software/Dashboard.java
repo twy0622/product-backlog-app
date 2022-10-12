3ce906e20d21b2e6cd89afeb43fc9b72c1e61a9a
yeevern <yeevernchang@gmail.com>
2022-10-12 09:49:26 +0800

    For Dashboard user story, implemented backend for the Start Date and End Date input fields.

--- Projects/Software/app/src/main/java/com/example/software/Dashboard.java
+++ Projects/Software/app/src/main/java/com/example/software/Dashboard.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModelProvider;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -34,29 +35,49 @@ public class Dashboard extends AppCompatActivity {
 
 //        mTaskViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
 
-        DatePickerDialog.OnDateSetListener date = (view, year, month, day) -> {
+        DatePickerDialog.OnDateSetListener start_date = (view, year, month, day) -> {
             myStartCalendar.set(Calendar.YEAR, year);
             myStartCalendar.set(Calendar.MONTH, month);
             myStartCalendar.set(Calendar.DAY_OF_MONTH, day);
-            updateLabel();
+            updateStartLabel();
         };
-        startDateInput.setOnClickListener(view -> new DatePickerDialog(Dashboard.this,date,myStartCalendar.get(Calendar.YEAR),myStartCalendar.get(Calendar.MONTH),myStartCalendar.get(Calendar.DAY_OF_MONTH)).show());
-        endDateInput.setOnClickListener(view -> new DatePickerDialog(Dashboard.this,date,myEndCalendar.get(Calendar.YEAR),myEndCalendar.get(Calendar.MONTH),myEndCalendar.get(Calendar.DAY_OF_MONTH)).show());
+        DatePickerDialog.OnDateSetListener end_date = (view, year, month, day) -> {
+            myEndCalendar.set(Calendar.YEAR, year);
+            myEndCalendar.set(Calendar.MONTH, month);
+            myEndCalendar.set(Calendar.DAY_OF_MONTH, day);
+            updateEndLabel();
+        };
+        startDateInput.setOnClickListener(view -> new DatePickerDialog(Dashboard.this,start_date,myStartCalendar.get(Calendar.YEAR),myStartCalendar.get(Calendar.MONTH),myStartCalendar.get(Calendar.DAY_OF_MONTH)).show());
+        endDateInput.setOnClickListener(view -> new DatePickerDialog(Dashboard.this,end_date,myEndCalendar.get(Calendar.YEAR),myEndCalendar.get(Calendar.MONTH),myEndCalendar.get(Calendar.DAY_OF_MONTH)).show());
 
-        if (myStartCalendar.get(Calendar.MONTH) == myEndCalendar.get(Calendar.MONTH)) {
-            dateRange = myStartCalendar.get(Calendar.DAY_OF_MONTH) - myEndCalendar.get(Calendar.DAY_OF_MONTH) + 1;
-        } else {
-            int lastDateOfMonth = myStartCalendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-            dateRange = lastDateOfMonth + myEndCalendar.get(Calendar.DAY_OF_MONTH) - myStartCalendar.get(Calendar.DAY_OF_MONTH) + 1;
-        }
-        System.out.println(dateRange);
+        Button selectRange = findViewById(R.id.selectRange);
+        selectRange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (myStartCalendar.get(Calendar.MONTH) == myEndCalendar.get(Calendar.MONTH)) {
+                    dateRange = myEndCalendar.get(Calendar.DAY_OF_MONTH) - myStartCalendar.get(Calendar.DAY_OF_MONTH) + 1;
+                } else {
+
+                    int difference = myEndCalendar.get(Calendar.MONTH) - myStartCalendar.get(Calendar.MONTH);
+                    int lastDateOfMonth = myStartCalendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+                    int bounded = lastDateOfMonth * difference;
+                    dateRange = bounded + myEndCalendar.get(Calendar.DAY_OF_MONTH) - myStartCalendar.get(Calendar.DAY_OF_MONTH) + 1;
+                }
+                Toast.makeText(getApplicationContext(),String.valueOf(dateRange),Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
-    private void updateLabel () {
+    private void updateStartLabel () {
         String myFormat = "dd/MM/yyyy";
         SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
         startDateInput.setText(dateFormat.format(myStartCalendar.getTime()));
+    }
+    private void updateEndLabel () {
+        String myFormat = "dd/MM/yyyy";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.US);
         endDateInput.setText(dateFormat.format(myEndCalendar.getTime()));
     }
 }
